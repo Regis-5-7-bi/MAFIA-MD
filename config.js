7acd9f13e43fa7565b5763851aee86acda8c804e
Regis-5-7-bi <nembotnembot237@gmail.com>
2025-07-29 12:27:23 +0100

    Update config.js
    Cool

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id here", 
+SESSION_ID: process.env.SESSION_ID || "MAFIA~MD~3toVmS6L#xheXKzVJeJ6fICie4JrzHwYL452qk0ZxM8G7tbfkPtE", 
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2250501889640",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "237698054605",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ᴇᴍᴘᴇʀᴏʀ sᴜᴋᴜɴᴀ",
 // add bot owner name
@@ -59,17 +59,17 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "2250501889640",
+DEV: process.env.DEV || "237698054605",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
